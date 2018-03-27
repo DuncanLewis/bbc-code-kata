7d9fc9937fd9b30bcd8489c6b2be14f802568336
Duncan Lewis <duncan@piranhr.com>
2018-03-27 12:38:27 +0100

    Can test that consumer called class constructor

--- romanNumeral-class-consumer.test.js
+++ romanNumeral-class-consumer.test.js
@@ -1,11 +1,14 @@
-import RomanNumeralGeneratorConsumer from './romanNumeral-class-consumer';
-import RomanNumeralGenerator from './romanNumeral-class';
+import RomanNumeralGeneratorConsumer from './romanNumeral-class-consumer'; // Import our consumer (interface)
+import RomanNumeralGenerator from './romanNumeral-class'; // Import our class itself
 
 jest.mock('./romanNumeral-class');
 
 let romanNumeralClassConsumer;
-const mockedMethodImpl = jest.fn();
+const mockedMethodImpl = jest.fn(); // To mock classes with jest we must use jest.fn();
 
+/*
+* mock our class before every test using mockedMethodImpl
+*/
 beforeAll(() => {
   RomanNumeralGenerator.mockImplementation(() => {
     return {
@@ -14,12 +17,26 @@ beforeAll(() => {
   })
 });
 
+/*
+* clear out mocks before every test run
+*/
 beforeEach(() => {
   RomanNumeralGenerator.mockClear();
   mockedMethodImpl.mockClear();
 })
 
+/*
+* First test that the class itself can be created
+*/
 it('The RomanNumeralGeneratorConsumer instance can be created', () => {
   const romanNumeralClassConsumer = new RomanNumeralGeneratorConsumer();
   expect(romanNumeralClassConsumer).toBeTruthy();
 })
+
+/*
+* Next we will test that the consumer did call class constructor
+*/
+it('The RomanNumeralGeneratorConsumer instance called the class constructor', () => {
+  const romanNumeralClassConsumer = new RomanNumeralGeneratorConsumer();
+  expect(RomanNumeralGenerator).toHaveBeenCalled();
+});
